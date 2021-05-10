368c4da4174590f4cd04e170909870679bfeebe3
Davronbek Achilov <davron211@gmail.com>
2021-05-10 10:53:14 +0500

    Position change news

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <home-banner />
     <about />
     <courses-list />
-    <news class="js-scroll fade-in-top" />
     <services class="js-scroll fade-in-top" />
     <finished-courses class="js-scroll fade-in-top" />
+    <news class="js-scroll fade-in-top" />
     <partners class="js-scroll fade-in-top" />
   </div>
 </template>
